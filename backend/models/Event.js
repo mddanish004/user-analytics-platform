667e721ebd689cf1f71e4646ab7d4cd08bbdcbf5
Md Danish Ansari <m.danishansari004@gmail.com>
2026-06-19 12:13:17 +0530

    Established the MongoDB Atlas connection and created the Event model

---
+++ backend/models/Event.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema(
+  {
+    session_id: {
+      type: String,
+      required: true,
+    },
+
+    type: {
+      type: String,
+      required: true,
+      enum: ["page_view", "click"],
+    },
+
+    url: {
+      type: String,
+      required: true,
+    },
+
+    timestamp: {
+      type: Date,
+      required: true,
+    },
+
+    x: {
+      type: Number,
+    },
+
+    y: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Event = mongoose.model("Event", eventSchema);
+
+export default Event;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-import dotenv from "dotenv";
-dotenv.config();
-
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
+
+import connectDB from "./config/db.js";
 
 const app = express();
 
@@ -15,6 +15,17 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+startServer();
